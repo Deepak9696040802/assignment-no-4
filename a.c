80683209ce8613299288fea3d15e517b95612917
Deepak9696040802 <113194198+Deepak9696040802@users.noreply.github.com>
2022-11-22 16:22:48 +0530

    Update a.c

--- a.c
+++ a.c
@@ -1,10 +1,20 @@
-tgybh
-kok
-k
-k
-k
-k
-k
-k
-k
-khf
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,count=0;
+ char s[20],c;
+ printf("enter a string : ");
+ gets(s);
+ for(c='a';c<='z';c++)
+ {
+    count=0;
+    for(i=0;s[i];i++)
+    {
+        if(c==s[i])
+        count++;
+    }
+    if(count>0)
+     printf("variable %c is in %d \n",c,count);
+ }
+}
